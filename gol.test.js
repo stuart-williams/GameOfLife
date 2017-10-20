8201d2f2fd59ab9bffc7106e9281b6d785278881
Stuart Williams <stuart.williams@bbc.co.uk>
2017-10-20 15:56:14 +0100

    Added readme

--- gol.test.js
+++ gol.test.js
@@ -12,10 +12,19 @@ describe(`Glider ${prettify([[1, 0, 0], [0, 1, 1], [1, 1, 0]])}`, () => {
       [0, 1, 0],
       [0, 0, 1],
       [1, 1, 1]
+    ],
+    [
+      [1, 0, 1],
+      [0, 1, 1],
+      [0, 1, 0]
     ]
   ]
 
   it('Glider 1', () => {
     expect(prettify(getGeneration(cells[0], 1))).toEqual(prettify(cells[1]))
+  })
+
+  it('Glider 2', () => {
+    expect(prettify(getGeneration(cells[0], 2))).toEqual(prettify(cells[2]))
   })
 })
