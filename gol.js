fa5ab6f15bed9121d4609e5e47db3ca2b64f8d43
Stuart Williams <stuart.williams@bbc.co.uk>
2017-10-23 09:57:39 +0100

    Tided

--- gol.js
+++ gol.js
@@ -1,3 +1,5 @@
+const columnContainsLiveCells = (cells, n) => cells.map((row) => row[n]).includes(1)
+
 const evolveCell = (isAlive, liveNeighbours) =>
   (isAlive && liveNeighbours === 2) || liveNeighbours === 3 ? 1 : 0
 
@@ -10,20 +12,23 @@ function countLiveNeighbours (cells, x, y) {
     count + (cells[x] && cells[x][y]) || count, 0)
 }
 
-const colHasLiveCells = (cells, n) => cells.map((row) => row[n]).includes(1)
-
 function trim (cells) {
   const cellsEnd = cells.length - 1
   const rowEnd = cells[0].length - 1
 
   const newCells = cells.reduce((accum, row, i) => {
-    if (!colHasLiveCells(cells, 0)) row = row.slice(1)
-    if (!colHasLiveCells(cells, rowEnd)) row = row.slice(0, -1)
+    if (!columnContainsLiveCells(cells, 0)) row = row.slice(1)
+    if (!columnContainsLiveCells(cells, rowEnd)) row = row.slice(0, -1)
     if ((i === 0 || i === cellsEnd) && !row.includes(1)) return accum
     return [ ...accum, row ]
   }, [])
 
-  if (!colHasLiveCells(newCells, 0) || !colHasLiveCells(newCells, newCells[0].length - 1) || !newCells[0].includes(1) || !newCells[newCells.length - 1].includes(1)) {
+  if (
+    !columnContainsLiveCells(newCells, 0) ||
+    !columnContainsLiveCells(newCells, newCells[0].length - 1) ||
+    !newCells[0].includes(1) ||
+    !newCells[newCells.length - 1].includes(1)
+  ) {
     return trim(newCells)
   }
 
